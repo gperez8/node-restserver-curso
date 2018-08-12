c7de1680d30c3bc1e6653dbe38b50d409d6eaac0
gperez8 <gregory.facyt@gmail.com>
2018-08-12 13:03:45 -0400

    Se realizo la peticion get tipo paginacion

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 const Usuario = require('../models/usuarios');
 
 const app = express();
 
 app.get('/usuario', function (req, res) {
-    res.json('get Usuario LOCAL!!!!')
+
+	const desde = Number(req.query.desde) || 0;
+	const limite = Number(req.query.limite) || 5;
+
+	Usuario.find({}, 'nombre email password')
+		.skip(desde)
+		.limit(limite)
+		.exec((err, usuarioBD) => {
+			if (err) {
+				return res.status(400).json({
+					ok: false,
+					err,
+				})
+			}
+
+			Usuario.find({}).count((err, total) => {
+				res.json({
+					ok: true,
+					usuarioBD,
+					total,
+				});
+			})
+		});
 })
   
 app.post('/usuario', function (req, res) {
@@ -32,8 +55,30 @@ app.post('/usuario', function (req, res) {
 	});
 });
 
-app.put('/usuario', function (req, res) {
-	res.json('get Usuario');
+app.put('/usuario/:id', function (req, res) {
+
+	const id = req.params.id;
+	const body = _.pick(req.body, ['nombre', 'email', 'img','role', 'estado']);
+
+	console.log(body);
+
+	console.log('id', id);
+	console.log('body', body);
+
+
+	Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioBD) => {
+		if (err) {
+			return res.status(400).json({
+				ok: false,
+				err,
+			});
+		}
+
+		res.json({
+			ok: true,
+			usuario: usuarioBD,
+		});
+	});
 });
 
 app.delete('/usuario', function (req, res) {
