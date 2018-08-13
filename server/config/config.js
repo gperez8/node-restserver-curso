5cbcda23118b4ee6b47d8b5febf8bca632c087ed
gperez8 <gregory.facyt@gmail.com>
2018-08-12 20:24:36 -0400

    variable de entorno a la BD

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb://cafe-gregory:C123456@ds119652.mlab.com:19652/cafe-nodejs';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.UrlBD = urlBD;
